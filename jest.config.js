96a3b3fd0f2b5a24493466c9e5fc9babd2b549b6
Amanda Oliveira <amandaol7b@gmail.com>
2024-12-16 11:20:02 -0300

    Initial jest tests

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  testEnvironment: 'jsdom', // Simula um navegador no Node.js
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'], // Ignora os diretórios node_modules e .next
+  collectCoverage: true, // O Jest irá gerar relatorios de cobertura, mostrando quais partes do código foram ou não testadas
+  collectCoverageFrom: ['src/**/*.ts(x)?'], // Especifica quais arquivos devem ser incluídos na análise de cobertura
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'], // Especifica um arquivo que será executado após o ambiente de teste ser configurado.
+  modulePaths: ['<rootDir>/src/'], //Adiciona src como um caminho de módulo base.
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  }
+  // Configura o Jest para transformar arquivos antes de testá-los:
+
+  // ^.+\\.(js|jsx|ts|tsx)$: Indica que todos os arquivos com extensões .js, .jsx, .ts, ou .tsx serão transformados.
+  // babel-jest: Usa o Babel para transpilar o código durante os testes.
+  // { presets: ['next/babel'] }: Aplica o preset do Babel usado pelo Next.js, garantindo que os testes lidem corretamente com as features modernas do JavaScript/TypeScript e JSX.
+}
