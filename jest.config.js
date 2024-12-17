79076fdbe71c36382dab9a9f419495cb1929b309
Amanda Oliveira <amandaol7b@gmail.com>
2024-12-16 21:49:29 -0300

    Add plop to generate files

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,10 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.ts(x)?',
     '!src/app/**',
-    '!src/lib/registry.tsx'
+    '!src/lib/registry.tsx',
+    '!src/types/**',
+    '!src/**/stories.tsx',
+    '!src/styles/**'
   ], // Especifica quais arquivos devem ser incluídos na análise de cobertura
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'], // Especifica um arquivo que será executado após o ambiente de teste ser configurado.
   modulePaths: ['<rootDir>/src/'], //Adiciona src como um caminho de módulo base.
@@ -25,10 +28,14 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
   // Configura o Jest para transformar arquivos antes de testá-los:
 
   // ^.+\\.(js|jsx|ts|tsx)$: Indica que todos os arquivos com extensões .js, .jsx, .ts, ou .tsx serão transformados.
   // babel-jest: Usa o Babel para transpilar o código durante os testes.
   // { presets: ['next/babel'] }: Aplica o preset do Babel usado pelo Next.js, garantindo que os testes lidem corretamente com as features modernas do JavaScript/TypeScript e JSX.
+  moduleNameMapper: {
+    '^styled-components':
+      'styled-components/dist/styled-components.browser.cjs.js'
+  }
 }
